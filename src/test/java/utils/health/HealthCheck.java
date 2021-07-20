1b0b609b97e1b6988941f9472361cca215cf5d2e
Mateusz <mateuszmuzyka@outlook.com>
2021-07-20 17:20:23 +0200

    add logger for HealthCheck

--- src/test/java/utils/health/HealthCheck.java
+++ src/test/java/utils/health/HealthCheck.java
@@ -1,5 +1,7 @@
 package utils.health;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -10,10 +12,15 @@ import static io.restassured.RestAssured.get;
  */
 public final class HealthCheck {
 
+    private final static Logger logger = Logger.getLogger(HealthCheck.class);
+
     @Test
     @Parameters({"environment"})
     public void healthCheck(String environment) {
+        BasicConfigurator.configure();
+        logger.info("Running health check...");
         int statusCode = checkAppsAvailability(environment);
+        logger.info("Health check ended with status code: " + statusCode);
         if (statusCode != 200) {
             System.out.println("Health check failed with status code:" + statusCode + " Tests will not run.");
             System.exit(0);
